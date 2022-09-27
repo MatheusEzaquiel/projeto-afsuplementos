f4cb51090e8187b625e29c5a6ebc22cff6d7eb2e
MatheusEzaquiel <matheusbezaquiel@gmail.com>
2022-09-27 18:57:30 -0300

    Nova página de carrinho

--- cliente/paginas/conteudo/carrinho.php
+++ cliente/paginas/conteudo/carrinho.php
@@ -23,6 +23,7 @@
                             <th>Produto</th>
                             <th>Preço</th>
                             <th>Quantidade</th>
+                            <th>Preço</th>
                             <th>Total</th>
                             <th>Remover</th>
                         </tr>
@@ -31,7 +32,10 @@
                     <?php
                         //Select dos produtos
                         include_once("../../config/conexao.php");
-                        $selCarrinho = "SELECT * FROM tb_carrinho";
+                        $selCarrinho = "SELECT * FROM tb_carrinho
+                        INNER JOIN tb_cliente ON tb_cliente.id_cliente = tb_carrinho.cliente_pedido
+                        INNER JOIN tb_produto ON tb_produto.id_produto = tb_carrinho.produto_pedido";
+                        
                         
                         try {
                             $resultSelCarrinho = $conect->prepare($selCarrinho);
@@ -39,19 +43,20 @@
                             $contSelCarrinho = $resultSelCarrinho->rowCount();
 
                             if($contSelCarrinho > 0){
-                                while($showCarrinho = $resultSelCarrinho->FETCH(PDO::FETCH_OBJ)){
-                                    $showCarrinho->produto_pedido;
-                                    $showCarrinho->cliente_pedido;
-                                    $showCarrinho->preco_pedido;
-                                    $showCarrinho->estado_pedido;
+                                while($carrinho = $resultSelCarrinho->FETCH(PDO::FETCH_OBJ)){
+                                    $idP = $carrinho->id_produto;
+                                    $imgP = $carrinho->foto_produto;
+                                    $nomeP = $carrinho->nome_produto;
+                                    $precoP = $carrinho->preco_venda_produto;
+                                    $nomeC = $carrinho->nome_cliente;
+                                    //$qtdP = $carrinho->quantidade_produto;
+                                    
 
-                                        /*$selectProdutos = "SELECT * FROM tb_produto WHERE disponibilidade_produto = 1" 
-                                        AND tb_carrinho.produt_pedido = tb_produto.id_produto; */
                                    
                     ?>
                         <tr>
-                            <td class="align-middle"><img src="img/product-1.jpg" alt="" style="width: 50px;"><?php #echo $showCarrinho->produto_pedido;?></td>
-                            <td class="align-middle"><?php echo $showCarrinho->produto_pedido;?></td>
+                            <td class="align-middle"><img src="../../imgs/produtos/<?php echo $imgP;?>" alt="" style="width: 50px;"></td>
+                            <td class="align-middle"><?php echo $nomeP;?></td>
                             <td class="align-middle">
                                 <div class="input-group quantity mx-auto" style="width: 100px;">
                                     <div class="input-group-btn">
@@ -59,7 +64,7 @@
                                         <i class="fa fa-minus"></i>
                                         </button>
                                     </div>
-                                    <input type="text" class="form-control form-control-sm bg-secondary border-0 text-center" value="<?php #echo $showCarrinho->quantidade_produto;?>">
+                                        <input type="text" name="qtdProduto" class="form-control form-control-sm bg-secondary border-0 text-center" value="10">
                                     <div class="input-group-btn">
                                         <button class="btn btn-sm btn-primary btn-plus">
                                             <i class="fa fa-plus"></i>
@@ -67,8 +72,14 @@
                                     </div>
                                 </div>
                             </td>
-                            <td class="align-middle">R$  <?php #echo $showCarrinho->preco_pedido;?></td>
-                            <td class="align-middle"><button class="btn btn-sm btn-danger"><i class="fa fa-times"></i></button></td>
+                            <td class="align-middle">R$  <?php echo $precoP;?></td>
+                            <td class="align-middle">R$  <?php  $qtdP = 10; echo $precoP * $qtdP;?></td>
+                            <!-- Botão Remover -->
+                            <td class="align-middle">
+                                <a href="home.php?pagina=deletar&?DelP=<?php echo $idP;?>">
+                                    <button class="btn btn-sm btn-danger"> <i class="fa fa-times"></i></button>
+                                </a>
+                            </td>
                         </tr>
                     <?php       
                                 
@@ -84,39 +95,6 @@
                 </table>
             </div>
 
-            <?php
-                
-                /*
-                $selectProdutos = "SELECT * FROM tb_produto WHERE disponibilidade_produto = 1";
-                
-                try {
-                    $resultSelProdutos = $conect->prepare($selectProdutos);
-                    $resultSelProdutos->execute();
-                    $contSelProdutos = $resultSelProdutos->rowCount();
-                    
-
-                    if($contSelProdutos > 0){
-                        while($showProdutos = $resultSelProdutos->FETCH(PDO::FETCH_OBJ)){
-                            $idProduto = $showProdutos->id_produto;
-                            $nomeP = $showProdutos->tipo_produto;
-                            $marcaP = $showProdutos->marca_produto;
-                            $nomeP = $showProdutos->nome_produto;
-                            $tamP = $showProdutos->tamanho_produto;
-                            $descP = $showProdutos->descricao_produto;
-                            $precoCP =$showProdutos->preco_compra_produto;
-                            $precoVP = $showProdutos->preco_venda_produto;
-                            $qtdP = $showProdutos->quantidade_produto;
-                            $fotoP = $showProdutos->foto_produto;
-                            $promoP = $showProdutos->promocao_produto;
-          
-                        }//Fim while
-                    }else{
-                        echo "ERRO!!";
-                    }
-                } catch (PDOException $erro) {
-                    echo "ERRO DE PDO SELECT -> ".$erro->getMessage();
-                }*/
-            ?>
             <div class="col-lg-4">
                 <form class="mb-30" action="">
                     <div class="input-group">
